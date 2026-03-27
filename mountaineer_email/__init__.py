dbdfa1ab5ae222aa7f2866c3098d9de4a5f01307
Pierce Freeman <piercefreeman@gmail.com>
2026-03-27 08:26:45 -0700

    Fix lints

--- mountaineer_email/__init__.py
+++ mountaineer_email/__init__.py
@@ -3,10 +3,6 @@ from mountaineer_email.controller import (
     EmailControllerBase as EmailControllerBase,
     EmailRenderBase as EmailRenderBase,
 )
-from mountaineer_email.daemons.send_email import (
-    SendEmail as SendEmail,
-    SendEmailInput as SendEmailInput,
-)
 from mountaineer_email.render import (
     EmailMetadata as EmailMetadata,
 )
